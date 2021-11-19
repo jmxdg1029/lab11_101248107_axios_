2f8d24c0d4eff2fccdee3ad932b850c4b13c9138
John Michael De Guzman <jmxdg1029@gmail.com>
2021-11-18 21:38:07 -0500

    second commit

--- lab11_101248107_axios_/src/PersonList.js
+++ lab11_101248107_axios_/src/PersonList.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {Row,Col, Container} from 'react-bootstrap'
+import {Row,Col,Container, Button} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import axios from 'axios'
 
@@ -27,26 +27,32 @@ export default class PersonList extends Component {
             <div>
                 {
                    this.state.persons.map(u => (
-                       <div style={{backgroundColor: '#48D1CC'}}>
-                         <p>{u.name.title} {u.name.first} {u.name.last} - {u.login.uuid}</p>
-                         <div>
-                             <Container>
-                                <Row>
-                                    <Col className="mt-5"><img src={u.picture.large}/></Col>
-                                    <Col>
-                                        <Row>User Name: {u.login.username}</Row>
-                                        <Row>Gneder: {u.gender}</Row>
-                                        <Row>Time Zone Description: {u.location.timezone.description}</Row>
-                                        <Row>Address: {u.location.street.number} {u.location.street.name}, {u.location.city}, {u.location.state}, {u.location.country} - {u.location.postcode}</Row>
-                                        <Row>Email: {u.email}</Row>
-                                        <Row>Birth Date and Age: {u.dob.date} ({u.dob.age}) </Row>
-                                        <Row>Register Date: {u.registered.date}</Row>
-                                        <Row>Phone#: {u.phone}</Row>
-                                        <Row>Cell#: {u.cell}</Row>
-                                    </Col>
-                                </Row>
-                             </Container>
-                         </div>
+                       <div>
+                        <div className="m-3 p-3" style={{backgroundColor: '#48D1CC', borderRadius:'5px'}}>
+                            <p className="pt-3" style={{fontWeight:'bold'}}>{u.name.title} {u.name.first} {u.name.last} - {u.login.uuid}</p>
+                            <hr/>
+                            <div>
+                                <Container>
+                                    <Row>
+                                        <Col  className="mt-5">
+                                            <Row><img style={{ borderRadius:'50%', width:'200px'}} src={u.picture.large}/></Row>
+                                        </Col>
+                                        <Col >
+                                            <Row className="mb-2">User Name: {u.login.username}</Row>
+                                            <Row className="mb-2">Gneder: {u.gender}</Row>
+                                            <Row className="mb-2">Time Zone Description: {u.location.timezone.description}</Row>
+                                            <Row className="mb-2">Address: {u.location.street.number} {u.location.street.name}, {u.location.city}, {u.location.state}, {u.location.country} - {u.location.postcode}</Row>
+                                            <Row className="mb-2">Email: {u.email}</Row>
+                                            <Row className="mb-2">Birth Date and Age: {u.dob.date} ({u.dob.age}) </Row>
+                                            <Row className="mb-2">Register Date: {u.registered.date}</Row>
+                                            <Row className="mb-2">Phone#: {u.phone}</Row>
+                                            <Row className="mb-2">Cell#: {u.cell}</Row>
+                                        </Col>
+                                    </Row>
+                                </Container>
+                            </div>
+                        </div>
+                        <hr/>
                        </div>
                    ))
                 }
